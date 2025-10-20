4df898d2d34379cec0011fcb1a5e7b28333bf61e
eager94 <>
2025-10-20 20:04:55 +0300

    updated index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Hexlet, Hexlet!';
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
